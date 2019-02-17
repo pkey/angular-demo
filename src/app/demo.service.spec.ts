bae4343eec3cd2a676a60d82f36f9759595f62cb
Paulius Kutka <kutka100@gmail.com>
2019-02-17 16:59:33 +0200

    Fixes tests

--- src/app/demo.service.spec.ts
+++ src/app/demo.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { DemoService } from './demo.service';
 
 describe('DemoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.
+    configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DemoService]
+    }));
 
   it('should be created', () => {
     const service: DemoService = TestBed.get(DemoService);
